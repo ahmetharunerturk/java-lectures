0cd086c5ff9e359c6fe11cb3209e5367223c25d1
Ahmet Harun Ertürk <is_ahmet@outlook.com>
2023-10-04 20:25:27 +0200

    data types

---
+++ data types/data_types.java
@@ -0,0 +1,31 @@
+public class data_types{
+     public static void main(String[] args) {
+        /*
+        byte -128:127 = 1 byte
+        short -32768:32767 = 2 byte
+        int = 4 byte
+        long = 8 byte
+        float = 4 byte
+        double = 8 byte
+        boolean
+        char
+        string  
+
+        */
+        
+        byte sayi = 100;
+        short sayi2=-32768;
+        int sayi3 = 1234567890;
+        long sayi4 = 923456789123101L;
+        float sayi5 = 25.5F;
+        double sayi6 = 25.5123123123123123123;
+        boolean durum = false;
+        char karakter = 'a';
+        String kelime = "Ahmet";
+
+
+
+        System.out.println(kelime);
+    
+    }
+}
